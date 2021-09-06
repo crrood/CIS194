907e62d4c1cdfea00bdafd8767d1ad762884ddcf
Colin Rood <crrood@gmail.com>
2021-09-06 16:01:05 -0700

    Binary tree insert function

--- HW2/app/LogAnalysis.hs
+++ HW2/app/LogAnalysis.hs
@@ -5,6 +5,8 @@ import Log
 import Text.Read
 import Data.Maybe
 
+-- Parsing methods
+
 parseMessageType :: [String] -> MessageType
 parseMessageType (code:severity:_)
   | code == "I" = Info
@@ -14,23 +16,9 @@ parseMessageType (code:severity:_)
   | otherwise   = error "invalid message type code"
 parseMessageType _ = error "incorrect number of message type values"
 
-isError :: MessageType -> Bool
-isError messageType =
-  case messageType of
-    Error _ -> True
-    _       -> False
-
 parseTimestamp :: [String] -> Int -> Int
 parseTimestamp messageArray messageTypeLength =
   read (messageArray !! messageTypeLength) :: Int
-
-isUnknown :: [String] -> Bool
-isUnknown message
-  | code == "I" || code == "W"        = False
-  | code == "E" && isNothing severity = True
-  | otherwise                         = True
-  where code      = message !! 0
-        severity  = readMaybe (message !! 1) :: Maybe Int
 
 parseMessage :: String -> LogMessage
 parseMessage message
@@ -53,9 +41,65 @@ parse :: String -> [LogMessage]
 parse logFile =
   map parseMessage (lines logFile)
 
+-- Utility functions
+
+isError :: MessageType -> Bool
+isError messageType =
+  case messageType of
+    Error _ -> True
+    _       -> False
+
+isUnknown :: [String] -> Bool
+isUnknown message
+  | code == "I" || code == "W"              = False
+  | code == "E" && not (isNothing severity) = False
+  | otherwise                               = True
+  where code      = message !! 0
+        severity  = readMaybe (message !! 1) :: Maybe Int
+
+-- Binary search tree
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) messageTree = messageTree
+insert logMessage Leaf = Node Leaf logMessage Leaf
+insert logMessage (Node leftTree nodeMessage rightTree)
+  | newTimestamp < nodeTimestamp  = Node (insert logMessage leftTree) nodeMessage rightTree
+  | otherwise                     = Node leftTree nodeMessage (insert logMessage rightTree)
+  where newTree         = insert logMessage Leaf
+        newTimestamp    = getTimestamp newTree
+        nodeTimestamp   = getTimestamp (insert nodeMessage Leaf)
+
+getTimestamp :: MessageTree -> Int
+getTimestamp Leaf = 0
+getTimestamp (Node _ (LogMessage _ timestamp _) _) = timestamp
+getTimestamp _ = error "bad timestamp in tree"
+
+-- Input / output
+
 main :: IO ()
 main = do
-  putStrLn (show (parseMessage "E 2 562 help help"))
-  putStrLn (show (parseMessage "W 9836 mice in the air"))
-  putStrLn (show (parseMessage "Unknown format"))
-  -- putStrLn (show (testParse parse 10 "error.log"))
+  putStrLn (show (
+    (parseMessage "W 9836 mice in the air") ==
+    (LogMessage Warning 9836 "mice in the air")))
+
+  putStrLn (show (
+    parseMessage "Unknown format" ==
+    Unknown "Unknown format"))
+
+  let logMessage = parseMessage "E 2 562 help help"
+  putStrLn (show (
+    logMessage ==
+    (LogMessage (Error 2) 562 "help help")))
+
+  let rootNode = insert logMessage Leaf
+  putStrLn (show (
+    rootNode ==
+    Node Leaf (LogMessage (Error 2) 562 "help help") Leaf))
+
+  putStrLn (show (
+    (insert (parseMessage "W 9836 mice in the air") rootNode) ==
+      Node Leaf (LogMessage (Error 2) 562 "help help") (Node Leaf (LogMessage Warning 9836 "mice in the air") Leaf)))
+
+  putStrLn (show (
+    (insert (parseMessage "W 1 mice in the air") rootNode) ==
+    Node (Node Leaf (LogMessage Warning 1 "mice in the air") Leaf) (LogMessage (Error 2) 562 "help help") Leaf))
